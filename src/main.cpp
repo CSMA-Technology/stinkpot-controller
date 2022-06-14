a9df291d5eff611e8715ccee4de2f7baff36095e
Stephanie Cruz <exscruzme@gmail.com>
2022-06-13 20:00:47 -0400

    web server to control position of servo

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,21 @@
 #include <ESP8266mDNS.h>
 #include <ESP8266WebServer.h>   // Include the WebServer library
 #include <LittleFS.h>   // Include the LittleFS library - SPIFFS is deprecated
+#include <Servo.h>
 
-#define LED 2
+#define LED 2 // LED on GPIO2
+#define ServoPin 14 // D5 --> GPIO14
 
 ESP8266WiFiMulti wifiMulti;
 
 ESP8266WebServer server(80);    // new web server that listens for HTTP requests on port 80
 
-// FLASH_MAP_SETUP_CONFIG(FLASH_MAP_MAX_FS);
+Servo myservo;
 
 void handleNotFound();
 void handleLED();
 bool handleFileRead(String path);
+void handleServo();
 
 void setup() {
   // put your setup code here, to run once:
@@ -24,14 +27,18 @@ void setup() {
   delay(10);
   Serial.println('\n');
 
-  wifiMulti.addAP("your_wifi_network", "your_wifi_password");
+  // Attach Servo
+  myservo.attach(ServoPin);
 
+  // Connect to WiFi
+  wifiMulti.addAP("your_wifi_network", "your_wifi_password");
   Serial.println("Connecting ...");
   while (wifiMulti.run() != WL_CONNECTED) {
     delay(250);
     Serial.print('.');
   }
 
+  // If connection successful, print details
   Serial.println('\n');
   Serial.print("Connected to:\t");
   Serial.println(WiFi.SSID());
@@ -44,15 +51,17 @@ void setup() {
     Serial.println("Error setting up the mDNS responder!!!!");
   }
 
-  LittleFS.begin(); // Start the File System
+  // Start the File System
+  LittleFS.begin();
 
+  // Web Server Setup & Initiliaziation
   server.onNotFound([]() {
     if (!handleFileRead(server.uri())) {
       handleNotFound();
     }
   });
   server.on("/LED", HTTP_POST, handleLED);
-
+  server.on("/motor/setPOS", handleServo);
   server.begin();
   Serial.println("HTTP server started");
 }
@@ -79,10 +88,20 @@ bool handleFileRead(String path) {
   }
   if (LittleFS.exists(path)) {
     File file = LittleFS.open(path, "r");
-    size_t sent = server.streamFile(file, "text/html");
+    server.streamFile(file, "text/html");
     file.close();
     return true;
   }
   Serial.println("\tFile not found");
   return false;
+}
+
+void handleServo() {
+  String POS = server.arg("servoPOS");
+  int pos = POS.toInt();
+  myservo.write(pos); // Tell servo to go to the given position
+  Serial.print("Servo angle: ");
+  Serial.println(pos);
+  digitalWrite(LED, !(digitalRead(LED)));
+  server.send(200, "text/plain");
 }
